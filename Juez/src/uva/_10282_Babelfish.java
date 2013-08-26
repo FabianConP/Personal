ea6c0570a01e35e8a91fe1f2a2ab230eb64a5c27
fabianele1-desktop <fabian.conpir@gmail.com>
2013-08-25 22:41:13 -0500

    Test commit

--- Juez/src/uva/_10282_Babelfish.java
+++ Juez/src/uva/_10282_Babelfish.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-
 public class _10282_Babelfish {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
