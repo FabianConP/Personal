97a4cde65f01058bc3602ab203b02459d8c353f2
fabianele1-desktop <fabian.conpir@gmail.com>
2013-09-16 20:56:42 -0500

    10496 - Collecting Beepers Modify names and delete vars

--- Juez/src/uva/_10496_Collecting_Beepers.java
+++ Juez/src/uva/_10496_Collecting_Beepers.java
@@ -1,4 +1,5 @@
 package uva;
+
 import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,21 +7,20 @@ import java.io.InputStreamReader;
 
 public class _10496_Collecting_Beepers {
 
-	public static int sizex, sizey;
 	public static Point list[], start;
-	public static boolean[] vis;
-	public static int min;
+	public static boolean[] visited;
+	public static int minPath;
 
 	public static void back(int length, int cur, int last) {
 		if (length == list.length - 1) {
-			if (cur + dis(list[last], start) < min)
-				min = cur + dis(list[last], start);
+			if (cur + dis(list[last], start) < minPath)
+				minPath = cur + dis(list[last], start);
 		} else
 			for (int i = 1; i < list.length; i++)
-				if (!vis[i]) {
-					vis[i] = true;
+				if (!visited[i]) {
+					visited[i] = true;
 					back(length + 1, cur + dis(list[last], list[i]), i);
-					vis[i] = false;
+					visited[i] = false;
 				}
 	}
 
@@ -35,22 +35,24 @@ public class _10496_Collecting_Beepers {
 		while ((line = in.readLine()) != null && line.length() != 0) {
 			int test = Integer.parseInt(line.trim()), v[];
 			for (int i = 0; i < test; i++) {
-				v = retInts(in.readLine());
-				sizex = v[0];
-				sizey = v[1];
+				in.readLine();
 				start = toPoint(retInts(in.readLine()));
 				int items = Integer.parseInt(in.readLine().trim());
 				list = new Point[items + 1];
 				list[0] = start;
-				vis = new boolean[items + 1];
+				visited = new boolean[items + 1];
 				for (int j = 1; j <= items; j++)
 					list[j] = toPoint(retInts(in.readLine()));
-				min = Integer.MAX_VALUE;
+				minPath = Integer.MAX_VALUE;
 				back(0, 0, 0);
-				out.append("The shortest path has length " + min + "\n");
+				out.append("The shortest path has length " + minPath + "\n");
 			}
 		}
 		System.out.print(out);
+	}
+
+	public static Point toPoint(int[] v) {
+		return new Point(v[0], v[1]);
 	}
 
 	public static int[] retInts(String line) {
@@ -61,7 +63,4 @@ public class _10496_Collecting_Beepers {
 		return a;
 	}
 
-	public static Point toPoint(int[] v) {
-		return new Point(v[0], v[1]);
-	}
 }
