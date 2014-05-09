76e1722333bf640cb110e3f312ce5bbcfeb52cee
Fabian <fabian.conpir@gmail.com>
2014-05-08 21:23:12 -0500

    11450 - Wedding shopping Down-Top DP

--- Juez/src/uva/_11450_Wedding_shopping.java
+++ Juez/src/uva/_11450_Wedding_shopping.java
@@ -1,5 +1,4 @@
 package uva;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,28 +12,28 @@ public class _11450_Wedding_shopping {
 			int times = Integer.parseInt(line.trim());
 			for (int i = 0; i < times; i++) {
 				int mc[] = readInts(in.readLine()), m = mc[0], c = mc[1];
-				int[][] it = new int[c + 1][m + 1];
-				int[][] co = new int[c + 1][m + 1];
-				int[] aux;
-				boolean pos = true;
-				for (int j = 1; j <= c; j++) {
+				boolean[][] cr = new boolean[m + 1][c];
+				int aux[], ans = -1;
+				for (int j = 0; j < c; j++) {
 					aux = readInts(in.readLine());
-					boolean buyAnItem = false;
-					for (int k = 1; k < aux.length && pos; k++) {
-						for (int l = aux[k]; l <= m; l++) {
-							if(it[j-1][l-aux[k]] == j-1) {
-								buyAnItem = true;
-								it[j][l] = Math.max(it[j][l], it[j-1][l-aux[k]]+1);
-								co[j][l] = Math.max(co[j][l], co[j-1][l-aux[k]]+aux[k]);
-							}
-						}
-					}
-					pos&=buyAnItem;
+					if (j == 0) {
+						for (int it = 1; it < aux.length; it++)
+							if (m - aux[it] >= 0)
+								cr[m - aux[it]][0] = true;
+					} else
+						for (int it = 1; it < aux.length; it++)
+							for (int p = m - aux[it]; p >= 0; p--)
+								cr[p][j] |= cr[p + aux[it]][j - 1];
 				}
-				if(!pos)
+				for (int j = 0; j <= m; j++)
+					if (cr[j][c - 1]) {
+						ans = m - j;
+						break;
+					}
+				if (ans == -1)
 					out.append("no solution\n");
 				else
-					out.append(co[c][m]+"\n");
+					out.append(ans + "\n");
 			}
 		}
 		System.out.print(out);
