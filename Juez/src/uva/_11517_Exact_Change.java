9990055565ed0a79f85b6e0d417e7d4728bf2e8c
Fabian <fabian.conpir@gmail.com>
2014-05-16 01:16:47 -0500

    11517 - Exact Change Sin matriz booleana

--- Juez/src/uva/_11517_Exact_Change.java
+++ Juez/src/uva/_11517_Exact_Change.java
@@ -3,37 +3,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class _11517_Exact_Change {
 	public static int V[], INF = Integer.MAX_VALUE / 2;
 
-	public static boolean[][] cr;
 	public static int[][] minCost;
 
 	public static int fillMinCost(int size, int amount) {
 		minCost = new int[V.length][size + 1];
 		for (int i = 0; i < minCost.length; i++)
 			Arrays.fill(minCost[i], INF);
-		cr = new boolean[V.length][size + 1];
 		int index = INF;
 		for (int i = 0; i < V.length; i++) {
 			if (i == 0) {
 				minCost[0][0] = 0;
-				cr[0][0] = true;
-				cr[0][V[i]] = true;
 				minCost[0][V[i]] = 1;
 				if (V[i] >= amount)
 					index = V[i];
 			} else
 				for (int j = 0; j <= size && j <= index; j++) {
-					if (j - V[i] >= 0 && cr[i - 1][j - V[i]]) {
-						cr[i][j] = true;
+					if (j - V[i] >= 0 && minCost[i - 1][j - V[i]]!=INF) {
 						minCost[i][j] = min(minCost[i - 1][j], minCost[i - 1][j
 								- V[i]] + 1, minCost[i][j]);
-					} else {
-						cr[i][j] = cr[i - 1][j];
+					} else 
 						minCost[i][j] = minCost[i - 1][j];
-					}
 					if (j >= amount && minCost[i][j] != INF) {
 						index = j;
 						if (i == V.length - 1)
