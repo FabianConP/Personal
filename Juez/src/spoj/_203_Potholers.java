9962838a86dcac1c6d64162ff159526b29884fce
FabianConP <fdconejop@unal.edu.co>
2014-09-27 13:58:37 -0500

    203. Potholers

---
+++ Juez/src/spoj/_203_Potholers.java
@@ -0,0 +1,97 @@
+package spoj;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class _203_Potholers {
+	private static class Edmons_karp {
+		public static int INF = 200000000;
+		public int g[][];
+		public ArrayList<Integer>[] ady;
+		public int mf, f, s, t;// max flow, actually flow, source node,
+								// finish node
+		public int V;// number of vertex
+		public int[] p;
+
+		public Edmons_karp(int v) {
+			V = v;
+			g = new int[V][V];
+			p = new int[V];
+			ady = new ArrayList[V];
+			for (int i = 0; i < ady.length; i++)
+				ady[i] = new ArrayList<Integer>(V);
+		}
+
+		private void augment(int v, int minEdge) {
+			if (v == s) {
+				f = minEdge;
+				return;
+			} else if (p[v] != -1) {
+				augment(p[v], Math.min(minEdge, g[p[v]][v]));
+				g[p[v]][v] -= f;
+				g[v][p[v]] += f;
+			}
+		}
+
+		public int edmons_karp(int s, int t) {
+			int u, e;
+			mf = 0;
+			Arrays.fill(p, -1);
+			while (true) {
+				f = 0;
+				Queue<Integer> q = new LinkedList<Integer>();
+				int[] dist = new int[V];
+				Arrays.fill(dist, INF);
+				dist[s] =  0;
+				q.offer(s);
+
+				Arrays.fill(p, -1);
+
+				while (!q.isEmpty()) {
+					u = q.poll();
+					if (u == t)
+						break;
+					for (int i = 0; i < ady[u].size(); i++) {
+						e = ady[u].get(i);
+						if (g[u][e] > 0 && dist[e] == INF) {
+							dist[e] = dist[u] + 1;
+							q.offer(e);
+							p[e] = u;// parent of vertex e is vertex u
+						}
+					}
+				}
+				augment(t, INF);
+				if (f == 0)
+					break;
+				mf += f;
+			}
+			return mf;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		StringBuilder out = new StringBuilder();
+		int nTest = in.nextInt();
+		Edmons_karp f;
+		for (int i = 0; i < nTest; i++) {
+			int nNodes = in.nextInt();
+			f = new Edmons_karp(nNodes);
+			for (int j = 0; j < nNodes - 1; j++) {
+				int nNeigh = in.nextInt(), t, v;
+				for (int k = 0; k < nNeigh; k++) {
+					t = in.nextInt() - 1;
+					v = (j == 0 || t == nNodes - 1) ? 1 : 1 << 20;
+					f.ady[j].add(t);
+					f.ady[t].add(j);
+					f.g[j][t] = v;
+				}
+			}
+			out.append(f.edmons_karp(0, nNodes - 1) + "\n");
+		}
+		System.out.print(out);
+	}
+}
