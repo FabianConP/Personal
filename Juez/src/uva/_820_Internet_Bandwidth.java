eeefb5bb126ea6b65dbe498a8f1766232fcbba35
FabianConP <fdconejop@unal.edu.co>
2014-09-27 18:56:16 -0500

    820 - Internet Bandwidth

---
+++ Juez/src/uva/_820_Internet_Bandwidth.java
@@ -0,0 +1,117 @@
+package uva;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class _820_Internet_Bandwidth {
+	
+	private static class Edmons_karp {
+		public static int INF = 200000000;
+		public int g[][];
+		public ArrayList<Integer>[] ady;
+		public int mf, f, s;// max flow, actually flow, source node,
+		public int V;// number of vertex
+		public int[] p;
+
+		public Edmons_karp(int v) {
+			V = v;
+			g = new int[V][V];
+			p = new int[V];
+			ady = new ArrayList[V];
+			for (int i = 0; i < ady.length; i++)
+				ady[i] = new ArrayList<Integer>(V);
+		}
+
+		private void augment(int v, int minEdge) {
+			if (v == s) {
+				f = minEdge;
+				return;
+			} else if (p[v] != -1) {
+				augment(p[v], Math.min(minEdge, g[p[v]][v]));
+				g[p[v]][v] -= f;
+				g[v][p[v]] += f;
+			}
+		}
+
+		public int edmons_karp(int s, int t) {
+			int u, e;
+			mf = 0;
+			this.s = s;
+			Arrays.fill(p, -1);
+			while (true) {
+				f = 0;
+				Queue<Integer> q = new LinkedList<Integer>();
+				int[] dist = new int[V];
+				Arrays.fill(dist, INF);
+				dist[s] = 0;
+				q.offer(s);
+
+				Arrays.fill(p, -1);
+
+				while (!q.isEmpty()) {
+					u = q.poll();
+					if (u == t)
+						break;
+					for (int i = 0; i < ady[u].size(); i++) {
+						e = ady[u].get(i);
+						if (g[u][e] > 0 && dist[e] == INF) {
+							dist[e] = dist[u] + 1;
+							q.offer(e);
+							p[e] = u;// parent of vertex e is vertex u
+						}
+					}
+				}
+				augment(t, INF);
+				if (f == 0)
+					break;
+				mf += f;
+			}
+			return mf;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line = "";
+		StringBuilder out = new StringBuilder();
+		Edmons_karp f;
+		int nNet = 1;
+		while ((line = in.readLine()) != null) {
+			int nNodes = Integer.parseInt(line.trim());
+			if (nNodes == 0)
+				break;
+			out.append("Network " + nNet++ + "\n");
+			f = new Edmons_karp(nNodes);
+			int[] info = readInts(in.readLine());
+			int s = info[0] - 1, t = info[1] - 1, c = info[2], a, b, w;
+			for (int j = 0; j < c; j++) {
+				info = readInts(in.readLine());
+				a = info[0] - 1;
+				b = info[1] - 1;
+				w = info[2];
+				if (f.g[a][b] == 0) {
+					f.ady[a].add(b);
+					f.ady[b].add(a);
+				}
+				f.g[a][b] += w;
+				f.g[b][a] += w;
+			}
+			int mf = f.edmons_karp(s, t);
+			out.append("The bandwidth is " + mf + ".\n\n");
+		}
+		System.out.print(out);
+	}
+
+	public static int[] readInts(String line) {
+		String[] w = line.trim().split(" ");
+		int[] a = new int[w.length];
+		for (int i = 0; i < w.length; i++)
+			a[i] = Integer.parseInt(w[i].trim());
+		return a;
+	}
+}
