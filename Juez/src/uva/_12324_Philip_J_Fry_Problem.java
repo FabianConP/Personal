a2110cb4d850ca000b5fc9209da31c8717019583
FabianConP <fdconejop@unal.edu.co>
2014-12-01 21:09:16 -0500

    12324 - Philip J. Fry Problem

--- Juez/src/uva/_12324_Philip_J_Fry_Problem.java
+++ Juez/src/uva/_12324_Philip_J_Fry_Problem.java
@@ -19,21 +19,20 @@ public class _12324_Philip_J_Fry_Problem {
 				d[i] = readInts(in.readLine());
 			int[][] t = new int[n][n];
 			int[][] am = new int[n][n];
-			Arrays.fill(t[0], d[0][0]);
-			Arrays.fill(am[0], d[0][1]);
-			for (int i = 1; i < n; i++) {
-				t[i][0] = t[i - 1][0] + d[i][0];
-				am[i][0] = am[i - 1][0] + d[i][1];
+			for (int i = 0; i < n; i++) {
+				t[0][i] = d[0][0];
+				am[0][i] = d[0][1];
 			}
-			for (int i = 1; i < n; i++)
-				for (int j = 1; j < n; j++) {
+			for (int i = 1; i < n; i++){
+				for (int j = 0; j < n; j++) {
 					t[i][j] = t[i - 1][j] + d[i][0];
 					am[i][j] = am[i - 1][j] + d[i][1];
-					if (am[i - 1][j - 1] > 0 && t[i - 1][j - 1] + d[i][0] / 2 < t[i][j]) {
+					if (j-1 >= 0 && am[i - 1][j - 1] > 0 && t[i - 1][j - 1] + d[i][0] / 2 < t[i][j]) {
 						t[i][j] = t[i - 1][j - 1] + d[i][0] / 2;
 						am[i][j] = am[i - 1][j - 1] + d[i][1] - 1;
 					}
 				}
+			}
 			int ans = Integer.MAX_VALUE;
 			for (int i = 0; i < n; i++)
 				ans = Math.min(ans, t[n - 1][i]);
