18f80047e4013ca964eaba9084f9067ccab82c5a
fabianlm17 <fabianlm17@HP>
2015-04-10 20:20:56 -0500

    531 - Compromise

--- Juez/src/uva/_531_Compromise.java
+++ Juez/src/uva/_531_Compromise.java
@@ -1,13 +1,14 @@
 package uva;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class _531_Compromise {
-	
+
 	public static boolean[][] match;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String line = "";
@@ -35,43 +36,35 @@ public class _531_Compromise {
 			}
 			int rows = a.size(), cols = b.size();
 			match = new boolean[rows][cols];
-			for (int i = 0; i < rows; i++) 
-				for (int j = 0; j < cols; j++) 
+			for (int i = 0; i < rows; i++)
+				for (int j = 0; j < cols; j++)
 					match[i][j] = a.get(i).equals(b.get(j));
-			int[][] lcs = new int[rows+1][cols+1];
-			for (int i = 1; i <= rows; i++) {
-				for (int j = 1; j <= cols; j++) {
-					if(match[i-1][j-1]){
-						lcs[i][j] = lcs[i-1][j-1]+1;
-					}else{
-						lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
-					}
-				}
-			}
+			int[][] lcs = new int[rows + 1][cols + 1];
+			for (int i = 1; i <= rows; i++)
+				for (int j = 1; j <= cols; j++)
+					if (match[i - 1][j - 1])
+						lcs[i][j] = lcs[i - 1][j - 1] + 1;
+					else
+						lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
 			String[] ans = new String[lcs[rows][cols]];
-			int r = rows, c = cols, count = lcs[rows][cols] -1;
-			while(r>0 &&  c>0){
-				if(match[r-1][c-1]){
-					ans[count--] = a.get(r-1);
+			int r = rows, c = cols, count = lcs[rows][cols] - 1;
+			while (r > 0 && c > 0)
+				if (match[r - 1][c - 1]) {
+					ans[count--] = a.get(r - 1);
 					r--;
 					c--;
-				}else if(lcs[r-1][c]>lcs[r][c-1])
+				} else if (lcs[r - 1][c] > lcs[r][c - 1])
 					r--;
 				else
 					c--;
-			}
 			for (int i = 0; i < ans.length; i++) {
-				if(i!=0)
+				if (i != 0)
 					out.append(" ");
 				out.append(ans[i]);
 			}
 			out.append("\n");
-			
 		}
 		System.out.print(out);
 	}
-	
-	static void recon(){
-		
-	}
+
 }
