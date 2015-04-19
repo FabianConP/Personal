ec02092e36b5fd8abdc1f264e30bbba5c1773ddd
fabianlm17 <fabianlm17@HP>
2015-04-18 23:00:35 -0500

    441 - Lotto

--- Juez/src/uva/_441_Lotto.java
+++ Juez/src/uva/_441_Lotto.java
@@ -3,45 +3,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class _441_Lotto {
 
 	public static int[] v;
 	public static StringBuilder out;
 
+	public static void solve(int ind, int l, String p) {
+		if (l == 6)
+			out.append(p + "\n");
+		else
+			for (int i = ind; i <= v.length - (6-l); i++) 
+				solve(i + 1, l + 1, p + " " + v[i]);
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String line = "";
 		out = new StringBuilder();
-		boolean start = true;
-		do {
-			line = in.readLine();
-			if (line == null || line.length() == 0)
+		int nCase = 1;
+		while ((line = in.readLine()) != null && line.length() != 0) {
+			if (line.equals("0"))
 				break;
-			int[] data = retInts(line);
-			if (data[0] == 0)
-				break;
-			if (!start)
+			v = readInts(line);
+			Arrays.sort(v,1,v.length);
+			if (nCase++ != 1)
 				out.append("\n");
-			start = false;
-			v = new int[data.length - 1];
-			Arrays.sort(v);
-			System.arraycopy(data, 1, v, 0, data.length - 1);
-			DFS(0, 0, "");
-		} while (line != null && line.length() != 0);
+			for (int i = 1; i <= v.length-6; i++)
+				solve(i + 1, 1, v[i]+"");
+
+		}
 		System.out.print(out);
 	}
 
-	public static void DFS(int i, int l, String path) {
-		if (l == 6)
-			out.append(path.trim() + "\n");
-		else
-			for (int j = i; j < v.length; j++) 
-				DFS(j+1, l+1, path+" "+v[j]);
-	}
-
-	public static int[] retInts(String line) {
-		String[] w = line.split(" ");
+	public static int[] readInts(String line) {
+		String[] w = Pattern.compile("\\s").split(line.trim());
 		int[] a = new int[w.length];
 		for (int i = 0; i < w.length; i++)
 			a[i] = Integer.parseInt(w[i]);
