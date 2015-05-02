878d3618e7ecc18b7058ae1c356e05077fbb1576
fabianlm17 <fabianlm17@HP>
2015-05-02 00:39:44 -0500

    11296 - Counting Solutions to an Integral Equation

---
+++ Juez/src/uva/_11296_Counting_Solutions_to_an_Integral_Equation.java
@@ -0,0 +1,22 @@
+package uva;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class _11296_Counting_Solutions_to_an_Integral_Equation {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line = "";
+		StringBuilder out = new StringBuilder();
+		BigInteger one = BigInteger.ONE;
+		BigInteger two = BigInteger.valueOf(2);
+		while ((line = in.readLine()) != null && line.length() != 0) {
+			int num = Integer.parseInt(line.trim())/2;
+			BigInteger n = BigInteger.valueOf(num);
+			BigInteger ans = ((n.add(one)).multiply(n.add(two))).divide(two);
+			System.out.println(ans);
+		}
+		System.out.print(out);
+	}
+}
